52ce7109afe0b7778406decf28fcf198e59b4636
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-10-13 18:38:21 +0200

    Add task which rank 3 most often repeated words in given set

---
+++ python/word-rank/script.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+from collections import Counter
+from operator import itemgetter, attrgetter
+
+# input: array with multiple strings
+# expected output: rank of the 3 most often repeated words in given set of strings and number of times they occured, case insensitive
+
+sentences = [
+    'Taki mamy klimat',
+    'Wszędzie dobrze ale w domu najlepiej',
+    'Wyskoczył jak Filip z konopii',
+    'Gdzie kucharek sześć tam nie ma co jeść',
+    'Nie ma to jak w domu',
+    'Konduktorze łaskawy zabierz nas do Warszawy',
+    'Jeżeli nie zjesz obiadu to nie dostaniesz deseru',
+    'Bez pracy nie ma kołaczy',
+    'Kto sieje wiatr ten zbiera burzę',
+    'Być szybkim jak wiatr',
+    'Kopać pod kimś dołki',
+    'Gdzie raki zimują',
+    'Gdzie pieprz rośnie',
+    'Swoją drogą to gdzie rośnie pieprz?',
+    'Mam nadzieję, że poradzisz sobie z tym zadaniem bez problemu',
+    'Nie powinno sprawić żadnego problemu, bo Google jest dozwolony',
+]
+
+
+def most_repeated_words(sentences: list, number_of_words: int) -> list[tuple]:
+    list_with_seperated_word = []
+    for x in sentences:
+        word_list = x.split()
+        list_with_seperated_word += word_list
+
+    result = Counter(list_with_seperated_word).most_common(number_of_words)
+    print(result)
+    return result
+
+
+def most_repeated_words_dict(sentences: list) -> list[tuple]:
+    list_with_seperated_word = []
+    words_dictionary = {}
+    for x in sentences:
+        word_list = x.split()
+        list_with_seperated_word += word_list
+
+    for word in list_with_seperated_word:
+        if word in words_dictionary.keys():
+            words_dictionary[word] += 1
+        else:
+            words_dictionary[word] = 0
+    result = sorted(words_dictionary.items(), key=itemgetter(1), reverse=True)
+    return result
+
+
+# most_repeated_words(sentences, 3)
+result = most_repeated_words(sentences, 3)
+print(f'1. "{result[0][0]}" - {result[0][1]}')
+print(f'2. "{result[1][0]}" - {result[1][1]}')
+print(f'3. "{result[2][0]}" - {result[2][1]}')
+# Example result:
+# 1. "mam" - 12
+# 2. "tak" - 5
+# 3. "z" - 2
+
+
+# Good luck! You can write all the code in this file.

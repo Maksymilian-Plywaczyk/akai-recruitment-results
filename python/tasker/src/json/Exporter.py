ea6291cf5dc7255b7bdd5bf3dca070fa81c58844
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-10-18 16:44:23 +0200

    Add typing to methods

--- python/tasker/src/json/Exporter.py
+++ python/tasker/src/json/Exporter.py
@@ -6,7 +6,7 @@ class Exporter:
     def __init__(self):
         pass
 
-    def save_tasks(self, tasks):
+    def save_tasks(self, tasks: list) -> None:
         # TODO zapisz taski do pliku tutaj
         with open('taski.json', 'w', encoding='utf-8') as file:
             json.dump(tasks, file, ensure_ascii=False)
--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -6,13 +6,13 @@ class Importer:
     def __init__(self):
         pass
 
-    def read_tasks(self):
+    def read_tasks(self) -> list:
         # TODO odczytaj plik i zdekoduj treść tutaj
         with open('taski.json', 'r', encoding='utf-8') as file:
             decode_json = json.load(file)
             return decode_json
 
-    def get_tasks(self):
+    def get_tasks(self) -> list:
         # TODO zwróć zdekodowane taski tutaj
         decode_tasks = self.read_tasks()
         return decode_tasks
